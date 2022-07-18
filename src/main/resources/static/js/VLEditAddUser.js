663101c7b163d814c8001fa673503c204d8d87ce
Vladislav A <validator1@yandex.ru>
2022-07-18 16:17:26 +0300

    done js for modal win 'edit/delete'

--- src/main/resources/static/js/VLEditAddUser.js
+++ src/main/resources/static/js/VLEditAddUser.js
@@ -31,15 +31,15 @@ function saveUser() {
             body.roles.push({id : role.value, name : role.text });
         }
     }
-    postParams(body)
+    postParams(body, "/admin/addUser")
     return false;
 }
 
 /////////////////////////////// с пом. fetch() //////////////////
-async function postParams(pbody) {
+async function postParams(pbody, pUrl) {
     try {
         console.log("before fetch()")
-        let response = await fetch("/admin/addUser", {
+        let response = await fetch(pUrl, {
             credentials: 'include',
             method: 'POST',
             headers: {
@@ -76,8 +76,14 @@ arrBtnEdit.forEach(function(elem) {
 }
 )
 
+const arrBtnDelete = document.querySelectorAll('.VLdelete');
+arrBtnDelete.forEach(function(elem) {
+        elem.onclick = btnDeleteOnClick;
+    }
+)
+
 async function btnEditOnClick() {
-    // by default fetch() user method GET:
+    // by default fetch() uses method GET:
     let json = null;
     let response  = await fetch("/admin/getUserWroles?id=" + this.value);
     if (response.ok) { // если HTTP-статус в диапазоне 200-299
@@ -88,17 +94,126 @@ async function btnEditOnClick() {
         alert("Ошибка HTTP: " + response.status);
     }
 
-    let rowID = "rowid" + this.value;
-    let cells = document.getElementById(rowID).getElementsByTagName("td");
-    $("#editId").val(cells[0].innerText);
+    $("#editId").val(json.id);
     // если не сработал jquery (а он несколько раз не работал для поля ID), то можно так:
     // document.getElementById("editId").value = cells[0].innerText;
-    $("#editFirstName").val(cells[1].innerText);
-    $("#editLastName").val(cells[2].innerText);
-    $("#editEmail").val(cells[3].innerText);
-    $("#editPassword").val(cells[4].innerText);
+    $("#editFirstName").val(json.firstName);
+    $("#editLastName").val(json.lastName);
+    $("#editEmail").val(json.email);
+    $("#editPassword").val(json.password);
+
+    let lvSelect = document.getElementById("editWinSelectRoles");
+    lvSelect.innerHTML = ""; // .empty(); // .append('<option value=1>My option</option>').selectmenu('refresh');
+    json.roles.forEach(pRole => {
+        let option = document.createElement("option");
+        option.value = pRole.id;
+        option.text = pRole.name;
+        if(pRole.checked)
+            option.selected = true;
+            // option.setAttribute('selected', "");
+        lvSelect.add(option, null);
+
+    });
 
 }
+
+////////////////////////////// btn Delete on click (modal window) /////////////////////////////////
+async function btnDeleteOnClick() {
+    // by default fetch() uses method GET:
+    let json = null;
+    let response  = await fetch("/admin/getUserWroles?id=" + this.value);
+    if (response.ok) { // если HTTP-статус в диапазоне 200-299
+        // получаем тело ответа (см. про этот метод ниже)
+        json = await response.json();
+        json.roles.sort(compareRoles)
+    } else {
+        alert("Ошибка HTTP: " + response.status);
+    }
+
+    $("#editId").val(json.id);
+    // если не сработал jquery (а он несколько раз не работал для поля ID), то можно так:
+    // document.getElementById("editId").value = cells[0].innerText;
+    $("#editFirstName").val(json.firstName).prop("readonly", true);
+    $("#editLastName").val(json.lastName).prop("readonly", true);
+    $("#editEmail").val(json.email).prop("readonly", true);
+    $("#editPassword").val(json.password).prop("readonly", true);
+
+    let lvSelect = document.getElementById("editWinSelectRoles");
+    lvSelect.innerHTML = ""; // .empty(); // .append('<option value=1>My option</option>').selectmenu('refresh');
+    json.roles.forEach(pRole => {
+        let option = document.createElement("option");
+        option.value = pRole.id;
+        option.text = pRole.name;
+        if(pRole.checked)
+            option.selected = true;
+        // option.setAttribute('selected', "");
+        lvSelect.add(option, null);
+
+    });
+    // lvSelect.ariaReadOnly = true;
+    // lvSelect.setAttribute("readonly", "true");
+    // lvSelect.setAttribute("readonly", true);
+    // $("#editWinSelectRoles").prop('readonly',true);
+    // $("#editWinSelectRoles").select2('readonly',true);
+
+    // $("#editWinSelectRoles").attr('disabled',true);
+
+    // $("#lvSelect").prop('disabled',true);
+    // $("#lvSelect").prop('disabled', "disabled");
+    // lvSelect.readonly = 1;
+    // $('#editWinSelectRoles').attr("style", "pointer-events: none;background-color:yellow");
+    // $("#editWinSelectRoles").readOnly = true;
+
+    let btnModalEdit = document.getElementById("btnModalEdit");
+    btnModalEdit.setAttribute("hidden", "");
+
+    let btnModalDelete = document.getElementById("btnModalDelete");
+    btnModalDelete.removeAttribute("hidden");
+    // $('#editWinSelectRoles').selectpicker('mobile');
+    // $('.selectpicker').selectpicker('mobile');
+}
+
+let btnModalEdit = document.getElementById("btnModalEdit");
+btnModalEdit.onclick = function (pEvent) {
+    let body = {}
+    body.id = document.getElementById("editId").value;
+    body.firstName = document.getElementById("editFirstName").value;
+    body.lastName = document.getElementById("editLastName").value;
+    body.email = document.getElementById("editEmail").value;
+    body.password = document.getElementById("editPassword").value;
+    body.roles = []
+
+    let roles = document.getElementById("editWinSelectRoles").options;
+    for(role of roles) {
+        if(role.selected){
+            body.roles.push({id : role.value, name : role.text });
+        }
+    }
+    postParams(body, "/admin/editUser");
+
+    $('#editDeleteUser').modal('hide');
+
+    return false;
+
+}
+
+let btnModalDelete = document.getElementById("btnModalDelete");
+btnModalDelete.onclick = btnModalDeleteOnclick;
+async function btnModalDeleteOnclick() {
+
+    $('#editDeleteUser').modal('hide');
+
+    let response = await fetch("/admin/delete?id=" + $("#editId").val())
+        .then(responce => {
+            if ( responce.redirected) {
+                window.location.href = responce.url;
+            }
+        });
+
+    return false;
+
+}
+
 
 function compareRoles( a, b ) {
     if ( a.id < b.id){
@@ -110,104 +225,9 @@ function compareRoles( a, b ) {
     return 0;
 }
 
-/* /////////////////получаем значения из ячеек по клику на кн. Edit ///////////////////////////////////
-// function btnEditOnClick() {
-//     let rowID = "rowid" + this.value;
-//     let cells = document.getElementById(rowID).getElementsByTagName("td");
-//     $("#editId").val(cells[0].innerText);
-//     // если не сработал jquery (а он несколько раз не работал для поля ID), то можно так:
-//     // document.getElementById("editId").value = cells[0].innerText;
-//     $("#editFirstName").val(cells[1].innerText);
-//     $("#editLastName").val(cells[2].innerText);
-//     $("#editEmail").val(cells[3].innerText);
-//     $("#editPassword").val(cells[4].innerText);
-//
-// }
-
- */
-
-/* /////////// можно использовать событие модального окна чтобы отловить нажатую кнопку: /////////////
-// $("#editDeleteUser").on("show.bs.modal",
-// function (event) {
-//             // так можно получить нажатую кнопку,  значение нажатой кнопки и т.д.:
-//             let ValueOfButton = event.relatedTarget.value
-//             alert("on(\"show.bs.modal\"");
-//         }
-// )
- */
-
-/*///////////// присвоение одной ф-ции нескольким элементам html в цикле://////////////
-// 1st var
-// Вам абсолютно правильно делают замечание, что ID в рамках страницы должен быть уникальным, но можно так:
-// document.querySelectorAll('[id="header"]');
-//2nd var:
-// делаем класс, например "__select" и далее:
-// const selects = document.querySelectorAll('.__select');
-// selects.forEach(function(select) {
-*/
-/* ///////////// обработка одной строки таблицы по клику по кнопке "edit" ///////
-// <table>
-//     <tr onClick="SetBackGround(this)">
-//         <td>1</td>
-//         <td>2</td>
-//         <td>3</td>
-//     </tr>
-//     <tr onClick="SetBackGround(this)">
-//         <td>4</td>
-//         <td>5</td>
-//         <td>6</td>
-//     </tr>
-//     <tr onClick="SetBackGround(this)">
-//         <td>7</td>
-//         <td>8</td>
-//         <td>9</td>
-//     </tr>
-// </table>
-// <script>
-//     function SetBackGround(sender)
-//     {
-//         var cells = sender.getElementsByTagName("td");
-//         for (var i = 0; i <= cells.length - 1;i++)
-//     {
-//         cells[i].style.backgroundColor = "#b9ffb9";
-//     }
-//     }
-// </script>
-*/
-
-/* ////////////some examples of fetch()////////////////////////////////
-// fetch('http://jsonplaceholder.typicode.com/users').then(function(response) {
-//     // response.json() returns a promise, use the same .then syntax to work with the results
-//     response.json().then(function(users){
-//         // users is now our actual variable parsed from the json, so we can use it
-//         users.forEach(function(user){
-//             console.log(user.name)
-//         });
-//     });
-// }).catch(err => console.error(err));
-//
-// ///////////////////////////
-// fetch('http://jsonplaceholder.typicode.com/users', {
-//     method: 'GET'
-// }).then((response) => {
-//     response.json().then((jsonResponse) => {
-//         console.log(jsonResponse)
-//     })
-//     // assuming your json object is wrapped in an array
-//     response.json().then(i => i.forEach(i => console.log(i.name)))
-// }).catch((err) => {
-//     console.log(`Error: ${err}` )
-// });
-// ////////////////////////////////
-// response.json() returns a promise, so you cannot just call forEach on it.
-//     Something more along the lines of
-// response.json().then(function(users){users.forEach(function(user){console.log(user.name)})
-
-
-///////////////////////// https://learn.javascript.ru/fetch
 
 
 
 
- */
+
 
